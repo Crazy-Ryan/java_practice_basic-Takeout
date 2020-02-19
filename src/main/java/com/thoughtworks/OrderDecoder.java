17a52c707f6bcc9f7da18c9e513abb86f9425aae
Ryan.Wang <ranwang1995@outlook.com>
2020-02-20 01:20:57 +0800

    [WangRan] feat: add class OrderDecoder to build order list

---
+++ src/main/java/com/thoughtworks/OrderDecoder.java
@@ -0,0 +1,40 @@
+package com.thoughtworks;
+
+import java.util.List;
+
+public class OrderDecoder {
+
+    private List<Dish> dishList;
+
+    private String selectedItems;
+
+    public OrderDecoder(List<Dish> dishList, String selectedItems) {
+        this.dishList = dishList;
+        this.selectedItems = selectedItems;
+    }
+
+    private Dish findDishById(String id) {
+        for (Dish dish : dishList) {
+            if (id.equals(dish.getId())) {
+                return dish;
+            }
+        }
+        return null;
+    }
+
+    public OrderList decode() {
+
+        OrderList orderList = new OrderList();
+        String[] selectedItemsArr = selectedItems.split(",");
+        for (String selectedItem : selectedItemsArr) {
+            String[] itemIdAndCount = selectedItem.split(" x ");
+            Dish dish = findDishById(itemIdAndCount[0]);
+            if (dish != null) {
+                orderList.addToList(new SingleDish(dish.getId(), dish.getName(), dish.getPrice(), Integer.parseInt(itemIdAndCount[1])));
+            }
+        }
+        return orderList;
+
+    }
+
+}
