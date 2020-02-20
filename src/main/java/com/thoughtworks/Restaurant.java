5c3273d06a8232d01f1f57d285e61028c380c643
Ryan.Wang <ranwang1995@outlook.com>
2020-02-20 23:44:26 +0800

    [WangRan] feat: add method to load order and find best promotion

--- src/main/java/com/thoughtworks/Restaurant.java
+++ src/main/java/com/thoughtworks/Restaurant.java
@@ -1,8 +1,31 @@
 package com.thoughtworks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Restaurant {
 
-  public String bestCharge(String selectedItems) {
-    return null;
-  }
+    public String bestCharge(String selectedItems) {
+        OrderList orderList = new OrderDecoder(DataProvider.getDishes(), selectedItems).decode();
+        ReceiptPrinter receiptPrinter = new ReceiptPrinter();
+        return receiptPrinter.print(findLowestCost(orderList));
+    }
+
+    private PromCal findLowestCost(OrderList orderList) {
+
+        List<PromCal> promList = new ArrayList<>();
+        promList.add(new NoPromCal(orderList));
+        promList.add(new ReachPromCal(orderList));
+        promList.add(new HalfPromCal(orderList));
+
+        PromCal promSelected = promList.get(0);
+        for (PromCal prom : promList) {
+            if (prom.getActualCost() < promSelected.getActualCost()) {
+                double promCost = prom.getActualCost();
+                double seleCost = promSelected.getActualCost();
+                promSelected = prom;
+            }
+        }
+        return promSelected;
+    }
 }
