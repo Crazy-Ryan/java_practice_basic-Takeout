59f5db05cca8cd81760d8d83fc0e8f37f2c5fe80
Ryan.Wang <ranwang1995@outlook.com>
2020-02-20 20:39:55 +0800

    [WangRan] feat: add class HalfPromCal to calculate the actual cost with specific dish offer 50% discount

---
+++ src/main/java/com/thoughtworks/HalfPromCal.java
@@ -0,0 +1,37 @@
+package com.thoughtworks;
+
+import java.util.List;
+
+public class HalfPromCal extends PromCal {
+
+    public HalfPromCal(OrderList orderList) {
+        super(orderList);
+    }
+
+    @Override
+    public void actualCostCal() {
+
+        double accumulator = 0;
+        for (SingleDish singleDish : orderList.getList()) {
+            double subtotal = singleDish.getPrice() * singleDish.getQuantity();
+            if (isInPromDishList(singleDish)) {
+                subtotal *= 0.5;
+                promDish.add(singleDish);
+            }
+            accumulator += subtotal;
+        }
+        actualCost = accumulator;
+        savedCost = cost-actualCost;
+    }
+
+    private boolean isInPromDishList(Dish dish) {
+        List<String> halfDishIds = DataProvider.getHalfDishIds();
+        for (String halfDishId : halfDishIds) {
+            if (halfDishId.equals(dish.getId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
