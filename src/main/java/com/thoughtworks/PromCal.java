51b003cf355bb8bbbd82367444c46755877565e5
Ryan.Wang <ranwang1995@outlook.com>
2020-02-20 20:10:02 +0800

    [WangRan] feat: add method to calculate total cost

--- src/main/java/com/thoughtworks/PromCal.java
+++ src/main/java/com/thoughtworks/PromCal.java
@@ -1,11 +1,41 @@
 package com.thoughtworks;
 
+import org.graalvm.compiler.lir.alloc.lsra.LinearScan;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class PromCal {
 
-    private double cost;
-    private double savedCost;
+    protected double cost;
+    protected double savedCost;
+    protected double actualCost;
+    protected OrderList orderList;
+    protected List<Dish> promDish;
 
-    public abstract void cal ();
+    public PromCal(OrderList orderList) {
+        this.orderList = orderList;
+    }
 
+    public double getCost() {
+        return cost;
+    }
+
+    public double getSavedCost() {
+        return savedCost;
+    }
+
+    public double getActualCost() {
+        return actualCost;
+    }
+
+    public abstract void actualCostCal ();
+    protected void costCal(){
+        double accumulator = 0;
+        for(SingleDish singleDish : orderList.getList()){
+            accumulator += singleDish.getPrice()*singleDish.getQuantity();
+        }
+        this.cost = accumulator;
+    }
 
 }
