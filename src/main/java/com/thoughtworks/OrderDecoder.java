86428a2a901f1aa00ba5fba9fa3f8ba3c7fc80ed
Ryan.Wang <ranwang1995@outlook.com>
2020-02-20 23:50:39 +0800

    [WangRan] refactor: remove unnecessary line breaks

--- src/main/java/com/thoughtworks/OrderDecoder.java
+++ src/main/java/com/thoughtworks/OrderDecoder.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class OrderDecoder {
 
     private List<Dish> dishList;
-
     private String selectedItems;
 
     public OrderDecoder(List<Dish> dishList, String selectedItems) {
